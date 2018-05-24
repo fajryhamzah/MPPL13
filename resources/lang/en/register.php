d247f06b5a073d674c602abada208f04c00997f8
fajryhamzah <fajryhamzah@gmail.com>
2018-05-24 13:58:45 +0700

    Redesign register page

--- resources/lang/en/register.php
+++ resources/lang/en/register.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+  'sign_up' => "Sign up",
+  'sub_sign_up' => "You can save life and be a hero",
+  'strong' => "Make a strong password",
   'exist' => "Username or Email already registered",
+  'sign_in' => "Already have an account? <a href='".url("register")."'>Sign in</a> here!",
 ];
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,76 @@
 
 @section("content")
 @include("layout.menu.main")
+<section id="log_in">
+  <div class="row">
+
+    <div class="col s12">
+        <div class="col s6" style="float:none;margin:0 auto;">
+          <div class="card" style="padding:5%;margin-top:20%">
+            <div class="card-content black-text">
+              <img src="{{ asset("images/logo2.png")}}" />
+              <span class="card-title">@lang("register.sign_up")</span>
+              <p>@lang("register.sub_sign_up")</p>
+
+              <form name="regist" method="post">
+
+                <div class ="row">
+                  <div class="input-field col s12">
+                    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailH" placeholder="your_email@your_email.com"/>
+                    <label for="email">Email</label>
+                  </div>
+                </div>
+
+                <div class ="row">
+                  <div class="input-field col s12">
+                    <input type="text" name="uname" class="form-control" id="uname" placeholder="username" />
+                    <label for="uname">Username</label>
+                  </div>
+                </div>
+
+                <div class ="row">
+                  <div class="input-field col s12">
+                    <input type="password" name="pass" class="form-control" id="pass" placeholder="@lang("register.strong")" />
+                    <label for="pass">Password</label>
+                  </div>
+                </div>
+
+                {{ csrf_field() }}
+                <div id="msg">
+                  @if(\Session::get("error"))
+                     @if(is_array(\Session::get("error")))
+                       <div class="alert alert-danger" role="alert">
+                         <ul>
+                         @foreach(\Session::get("error") as $a)
+                           <li>{{$a}}</li>
+                         @endforeach
+                         </ul>
+                       </div>
+                     @else
+                       <div class="alert alert-danger" role="alert">
+                         {{\Session::get("error")}}
+                       </div>
+                     @endif
+
+                  @endif
+
+
+                </div>
+                <div class ="row">
+                  <div class="col s12">
+                    <button type="submit" class="btn btn-primary">Register</button>
+                  </div>
+                </div>
+              </form>
+
+              <span>@lang("register.sign_in")</span>
+            </div>
+          </div>
+        </div>
+
+    </div>
+  </div>
+</section>
 <section class="sec">
   <div class="container">
     <div class="row">
@@ -13,8 +83,7 @@
             <small id="emailH" class="form-text text-muted">We use this for verification and notification.</small>
           </div>
           <div class="form-group">
-            <label for="uname">Username</label>
-            <input type="text" name="uname" class="form-control" id="uname" placeholder="username" />
+
           </div>
           <div class="form-group">
             <label for="pass">Password</label>
